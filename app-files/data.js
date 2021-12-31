da398f4a1249b2e93deff25f708dad6214005d30
merrygoldhl <hui_lin21@hotmail.com>
2021-12-31 23:52:27 +0800

    first commit

---
+++ app-files/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.945035318842443,
+        "pitch": -0.32330065213498216,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.042905238504755,
+          "pitch": 0.05294562684939308,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.807940716421859,
+        "pitch": -0.03921958241132728,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2990515774077283,
+          "pitch": 0.09127903001766313,
+          "rotation": 4.71238898038469,
+          "target": "0-alice-lee-plaza"
+        },
+        {
+          "yaw": -3.010705063638447,
+          "pitch": 0.038928272191091295,
+          "rotation": 1.5707963267948966,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.9590453942975685,
+        "pitch": -0.10145837188078843,
+        "fov": 1.3123694188512842
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32882534347010406,
+          "pitch": 0.07204264376214553,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Hui Lin_Assignment_YST",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
